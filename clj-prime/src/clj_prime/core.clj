667c1baaf316e7beae927a126e1b5234767257ea
Roberto Robles <robertorobles0@gmail.com>
2016-11-03 14:56:35 -0700

    First working version of clj-prime

---
+++ clj-prime/src/clj_prime/core.clj
@@ -0,0 +1,41 @@
+(ns clj-prime.core
+  (require [clojure.string :as string])
+  (:gen-class))
+
+(defn prime?
+  "Returns truthy value if number is prime"
+  [known-primes n]
+  {:pre [(and (vector? known-primes) (integer? n))]}
+  (not (some #(= 0 (mod n %)) known-primes)))
+
+(defn gen-prime-vec
+  "Prime numbers vector generator default size 10"
+  ([]
+   (gen-prime-vec 10))
+  ([max-size]
+   {:pre [(integer? max-size)] }
+   (loop [prime-vec [2]
+          next-to-check 3]
+     (if (= max-size (count prime-vec))
+       prime-vec
+       (if (prime? prime-vec next-to-check)
+         (recur (conj prime-vec next-to-check) (inc next-to-check))
+         (recur prime-vec (inc next-to-check)))))))
+
+(defn print-mult-table
+  "Prints the multiplication table of numbers in a vector"
+  [prime-vec]
+  {:pre [(vector? prime-vec)]}
+  (println "|   *X*   |   " (string/join "   |   " prime-vec) "   |")
+  (doseq [i prime-vec
+          :let [row (map #(* i %) prime-vec)]]
+    (println   "|   " i  "   |   " (string/join "   |   " row) "   |")))
+
+(defn -main
+  "Prints the multiplication table"
+  [& args]
+  (let [prime-vec (if (and (seq args)
+                           (re-matches #"^\d+$" (first args)))
+                    (gen-prime-vec (read-string (first args)))
+                    (gen-prime-vec))]
+    (print-mult-table prime-vec)))
---
+++ clj-prime/test/clj_prime/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-prime.core-test
+  (:require [clojure.test :refer :all]
+            [clj-prime.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
