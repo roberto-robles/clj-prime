8c52d757604f0388a54e955b3c019c6129e6fc50
Roberto Robles <robertorobles0@gmail.com>
2016-11-04 20:04:09 -0700

    Add lazy-seq strategy

--- src/clj_prime/core.clj
+++ src/clj_prime/core.clj
@@ -5,27 +5,44 @@
 (defn prime?
   "Returns truthy value if number is prime"
   [known-primes n]
-  {:pre [(and (vector? known-primes) (integer? n))]}
+  {:pre [(and (seq known-primes) (integer? n))]}
   (not (some #(= 0 (mod n %)) known-primes)))
 
-(defn gen-prime-vec
-  "Prime numbers vector generator default size 10"
+;; Slow, but doesn't raise Exceptions when we want a really long list of primes > 1500
+(defn- build-prime-vector
+  "Generates a vector of prime numbers with length 'size'"
+  [size]
+  {:pre [(integer? size)] }
+  (loop [prime-vec [2]
+         next-to-check 3]
+    (if (= size (count prime-vec))
+      prime-vec
+      (if (prime? prime-vec next-to-check)
+        (recur (conj prime-vec next-to-check) (inc next-to-check))
+        (recur prime-vec (inc next-to-check))))))
+
+(defn- build-prime-lazy-seq
+  "More efficient way of generating a list of prime numbers but not useful when
+   the lazy-seq that will be generated is larger than 1500 elements due to 
+   StackOverflowError safe to use when with (take 1500 ...)
+   This function spects that the initial seq starts with 2"
+  [s]
+  (cons (first s)
+        (lazy-seq (build-prime-lazy-seq (remove #(= 0 (mod % (first s))) (rest s))))))
+
+(defn prime-seq
   ([]
-   (gen-prime-vec 10))
-  ([max-size]
-   {:pre [(integer? max-size)] }
-   (loop [prime-vec [2]
-          next-to-check 3]
-     (if (= max-size (count prime-vec))
-       prime-vec
-       (if (prime? prime-vec next-to-check)
-         (recur (conj prime-vec next-to-check) (inc next-to-check))
-         (recur prime-vec (inc next-to-check)))))))
+   (prime-seq 10))
+  ([size]
+   {:pre [(integer? size)]}
+   (if (> size 1500)
+     (build-prime-vector size)
+     (take size (build-prime-lazy-seq (iterate inc 2))))))
 
 (defn print-mult-table
   "Prints the multiplication table of numbers in a vector"
   [prime-vec]
-  {:pre [(vector? prime-vec)]}
+  {:pre [(seq prime-vec)]}
   (println "|   *X*   |   " (string/join "   |   " prime-vec) "   |")
   (doseq [i prime-vec
           :let [row (map #(* i %) prime-vec)]]
@@ -37,8 +54,8 @@
    the first Nth prime numbers, otherwise we only show the table using
    the first 10 prime numbers."
   [& args]
-  (let [prime-vec (if (and (seq args)
+  (let [primes (if (and (seq args)
                            (re-matches #"^\d+$" (first args)))
-                    (gen-prime-vec (read-string (first args)))
-                    (gen-prime-vec))]
-    (print-mult-table prime-vec)))
+                 (prime-seq (read-string (first args)))
+                 (prime-seq))]
+    (print-mult-table primes)))
